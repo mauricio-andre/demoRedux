859f44938cf1820b36d6763faed8e3e96d68e7f0
dev03 <mauricio.r.andre@gmail.com>
2021-03-13 19:08:27 -0300

    Criando actions types

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -3,6 +3,7 @@ import { all, call, put, select, takeLatest } from 'redux-saga/effects';
 import { IState } from '../..';
 import api from '../../../services/api';
 import { addProductToCartFailure, addProductToCartRequest, addProductToCartSuccess } from './actions';
+import { ActionTypes } from './types';
 
 type CheckProductStockRequest = ReturnType<typeof addProductToCartRequest>;
 
@@ -34,5 +35,5 @@ function* checkProductStock({ payload }: CheckProductStockRequest) {
 }
 
 export default all([
-  takeLatest('ADD_PRODUCT_TO_CART_REQUEST', checkProductStock),
+  takeLatest(ActionTypes.addProductToCartRequest, checkProductStock),
 ]);
