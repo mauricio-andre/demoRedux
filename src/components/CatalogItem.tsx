e0d76d599fe3a70aa5aca9bc7d3726b114daaac9
dev03 <mauricio.r.andre@gmail.com>
2021-03-13 18:31:10 -0300

    Dividindo as actions

--- src/components/CatalogItem.tsx
+++ src/components/CatalogItem.tsx
@@ -1,6 +1,6 @@
 import { useCallback } from "react";
 import { useDispatch } from "react-redux";
-import { addProductToCart } from "../store/modules/cart/action";
+import { addProductToCartRequest } from "../store/modules/cart/actions";
 import { IProduct } from "../store/modules/cart/types";
 
 interface CatalogItemsProps {
@@ -11,7 +11,7 @@ const CatalogItem: React.FC<CatalogItemsProps> = ({ product }) => {
   const dispatch = useDispatch();
 
   const handleAddProductToCart = useCallback(() => {
-    dispatch(addProductToCart(product));
+    dispatch(addProductToCartRequest(product));
   }, [dispatch, product]);
 
   return (
--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,8 +1,8 @@
 import { all, takeLatest, select } from 'redux-saga/effects';
 import { IState } from '../..';
-import { addProductToCart } from './action';
+import { addProductToCartRequest } from './actions';
 
-type CheckProductStockRequest = ReturnType<typeof addProductToCart>;
+type CheckProductStockRequest = ReturnType<typeof addProductToCartRequest>;
 
 function* checkProductStock({ payload }: CheckProductStockRequest) {
   const { product } = payload;
@@ -15,4 +15,6 @@ function* checkProductStock({ payload }: CheckProductStockRequest) {
   });
 }
 
-export default all([takeLatest('ADD_PRODUCT_TO_CART', checkProductStock)]);
+export default all([
+  takeLatest('ADD_PRODUCT_TO_CART_REQUEST', checkProductStock),
+]);
